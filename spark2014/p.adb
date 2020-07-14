d4ae9d6be316bb0a7c4d2ea7072c7d97a7f44089
Rod Chapman <rod@proteancode.com>
2020-07-14 14:04:00 +0100

    First attempt at full proof of this package.

--- spark2014/p.adb
+++ spark2014/p.adb
@@ -1,58 +1,87 @@
 package body P
   with SPARK_Mode => On,
-       Refined_State => (State => S)
+       Refined_State => (State => (P1, P2, P3))
 is
-   type State_Type is record
-      P1 : Short_Integer;
-      P2 : Short_Integer;
-      P3 : Short_Integer;
-   end record;
+   M1  : constant := 30269;
+   M1F : constant := 30269.0;
+   R1  : constant := 171;
+   A1  : constant := 177;
+   B1  : constant := 2;
 
-   S : State_Type := (1, 2, 3);
+   M2  : constant := 30307;
+   M2F : constant := 30307.0;
+   R2  : constant := 172;
+   A2  : constant := 176;
+   B2  : constant := 35;
 
-   M1 : constant := 30269;
-   R1 : constant := 171;
-   A1 : constant := 177;
-   B1 : constant := 2;
+   M3  : constant := 30323;
+   M3F : constant := 30323.0;
+   R3  : constant := 170;
+   A3  : constant := 178;
+   B3  : constant := 63;
 
-   M2 : constant := 30307;
-   R2 : constant := 172;
-   A2 : constant := 176;
-   B2 : constant := 35;
+   subtype P1T is Short_Integer range 1 .. (M1 - 1);
+   P1 : P1T := 1;
 
-   M3 : constant := 30323;
-   R3 : constant := 170;
-   A3 : constant := 178;
-   B3 : constant := 63;
+   subtype P2T is Short_Integer range 1 .. (M2 - 1);
+   P2 : P2T := 2;
+
+   subtype P3T is Short_Integer range 1 .. (M3 - 1);
+   P3 : P3T := 3;
 
 
-   procedure Prang (X : out Long_Float)
+   procedure Prang (X : out Prang_Result)
+     with Refined_Global => (In_Out => (P1, P2, P3))
    is
-      Div1Q : constant Integer := Integer (S.P1) / Integer (A1);
-      Div1R : constant Integer := Integer (S.P1) rem Integer (A1);
-      Div2Q : constant Integer := Integer (S.P2) / Integer (A2);
-      Div2R : constant Integer := Integer (S.P2) rem Integer (A2);
-      Div3Q : constant Integer := Integer (S.P3) / Integer (A3);
-      Div3R : constant Integer := Integer (S.P3) rem Integer (A3);
+      Div1Q : constant Short_Integer := P1 / A1;
+      Div1R : constant Short_Integer := P1 rem A1;
+      Div2Q : constant Short_Integer := P2 / A2;
+      Div2R : constant Short_Integer := P2 rem A2;
+      Div3Q : constant Short_Integer := P3 / A3;
+      Div3R : constant Short_Integer := P3 rem A3;
 
-      Raux1,
-      Raux2,
-      Raux3 : Long_Float;
+      subtype Aux1T is Long_Float range 0.0 .. Long_Float (P1T'Last) / M1F;
+      Raux1 : Aux1T;
+
+      subtype Aux2T is Long_Float range 0.0 .. Long_Float (P2T'Last) / M2F;
+      Raux2 : Aux2T;
+
+      subtype Aux3T is Long_Float range 0.0 .. Long_Float (P3T'Last) / M3F;
+      Raux3 : Aux3T;
+
+      subtype Temp_T is Short_Integer
+        with Dynamic_Predicate => Temp_T /= 0;
+      T  : Temp_T;
+
+      subtype TXT is Long_Float range -Just_Below_One .. Just_Below_One;
+      TX : TXT;
+
+      function Remainder_1 (A : in Long_Float) return TXT
+      is
+      begin
+         --  Proof false alarm here since GNATProve does not know enough
+         --  about the mathematical properties of 'Remainder
+         return Long_Float'Remainder (A, 1.0);
+      end Remainder_1;
+
    begin
-      S := (P1 => R1 * Short_Integer (Div1R) - B1 * Short_Integer (Div1Q),
-            P2 => R2 * Short_Integer (Div2R) - B2 * Short_Integer (Div2Q),
-            P3 => R3 * Short_Integer (Div3R) - B3 * Short_Integer (Div3Q));
+      T  := R1 * Div1R - B1 * Div1Q;
+      P1 := T + (if T < 0 then M1 else 0);
+      Raux1 := Long_Float (P1) / M1F;
 
-      S.P1 := S.P1 + (if S.P1 < 0 then M1 else 0);
-      S.P2 := S.P2 + (if S.P2 < 0 then M2 else 0);
-      S.P3 := S.P3 + (if S.P3 < 0 then M3 else 0);
+      T  := R2 * Div2R - B2 * Div2Q;
+      P2 := T + (if T < 0 then M2 else 0);
+      Raux2 := Long_Float (P2) / M2F;
 
-      Raux1 := Long_Float (S.P1) / Long_Float (M1);
-      Raux2 := Long_Float (S.P2) / Long_Float (M2);
-      Raux3 := Long_Float (S.P3) / Long_Float (M3);
+      T  := R3 * Div3R - B3 * Div3Q;
+      P3 := T + (if T < 0 then M3 else 0);
+      Raux3 := Long_Float (P3) / M3F;
 
-      X := Long_Float'Remainder (Raux1 + Raux2 + Raux3, 1.0);
-      X := (if X < 0.0 then X + 1.0 else X);
+      TX := Remainder_1 (Raux1 + Raux2 + Raux3);
+
+      --  Clip upper-bound to Just_Below_One here to prevent TX + 1.0
+      --  rounding up to exactly 1.0
+      X := (if TX < 0.0 then Long_Float'Min (TX + 1.0, Just_Below_One) else TX);
    end Prang;
 
 
--- spark2014/p.ads
+++ spark2014/p.ads
@@ -3,8 +3,11 @@ package P
        Abstract_State => State,
        Initializes    => State
 is
+   Just_Below_One  : constant Long_Float := Long_Float'Adjacent (1.0, 0.0);
 
-   procedure Prang (X : out Long_Float)
+   subtype Prang_Result is Long_Float range 0.0 .. Just_Below_One;
+
+   procedure Prang (X : out Prang_Result)
      with Global => (In_Out => State);
 
 end P;
